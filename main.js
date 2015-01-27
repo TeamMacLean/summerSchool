9b457d3043a11eec7543b2a4466942fea0c35306
Martin Page <wookoouk@gmail.com>
2015-01-27 14:58:35 +0000

    TSL Summer School

---
+++ main.js
@@ -0,0 +1,38 @@
+// Google Maps
+function initialize_map() {
+
+    var place = {lat: 52.6224438, lng: 1.2228483};
+
+    var map = new google.maps.Map(document.getElementById("map-holder"), {
+        center: place,
+        zoom: 16,
+        scrollwheel: false,
+        draggable: true,
+        scaleControl: true,
+        navigationControl: false,
+        mapTypeControl: false,
+        streetViewControl: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var marker = new google.maps.Marker({
+        position: place,
+        icon: {
+            url: "pin.png",
+            scaledSize: new google.maps.Size(52, 52)
+        },
+        map: map
+    });
+
+    // Keep it centered on resize
+    window.onresize = function () {
+        google.maps.event.trigger(map, 'resize');
+        map.setCenter(place);
+    };
+
+}
+
+
+$(window).load(function () {
+    initialize_map();
+});
